2a954735388174a2da3f78435c4d62dc54ebde74
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-01-16 17:31:15 +0200

    webpack configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,30 @@
-const   path    = require('path'),
-        webpack = require('webpack');
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV.toLowerCase();
+
+const ENVIRONMENTS = {
+    DEV: 'dev',
+    STAGE: 'staging',
+    PROD: 'prod',
+    TEST: 'test',
+    DEBUG: 'debug'
+};
+
+const isDEBUG = NODE_ENV === ENVIRONMENTS.DEBUG
+    || (
+        NODE_ENV !== ENVIRONMENTS.STAGE
+        && NODE_ENV !== ENVIRONMENTS.PROD
+        && NODE_ENV !== ENVIRONMENTS.DEV
+        && NODE_ENV !== ENVIRONMENTS.DEBUG
+    );
+
+const config = {
+    app: __dirname + '/src/app',
+    dist: __dirname + '/dist'
+};
 
 const webpackConfig = {
     resolve: {
@@ -16,10 +41,26 @@ const webpackConfig = {
         path: __dirname + '/dist'
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            chunks: ['app', 'common'],
+            template: path.join(config.app, 'index.html')
+        }),
+
         new webpack.DefinePlugin({
             __API__: JSON.stringify(process.env.API)
-        })
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({ name: 'common' })
     ]
 };
+
+if (isDEBUG) {
+
+} else {
+    webpackConfig.plugins.push(
+        new CleanWebpackPlugin([config.dist], { verbose: true }),
+        new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+    )
+}
 
 module.exports = webpackConfig;
