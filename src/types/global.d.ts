a60465bef04e5933cc433e1e6f5cce6c297535bb
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-01-16 18:04:16 +0200

    webpack configuration

---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+declare const __BASE__: string;
+declare const __DEBUG__: string;
+declare const __API__: string;
+declare const module: { hot: { accept: Function } };
+declare var require: {
+    (path: string): any;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
